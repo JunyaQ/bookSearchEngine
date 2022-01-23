7d01f074a9ca6d6dbf521b6ebe4f53ea1182c936
Junya Qiao <junyaqiao@gmail.com>
2022-01-23 17:22:48 -0500

    fix graphql error

--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ const express = require('express');
 const path = require('path');
 //const db = require('./config/connection');
 const routes = require('./routes');
-//const {typeDefs,resolvers} = require('./schemas');
-
-//const {ApolloServer} = require("appollo-server-express");
+const {ApolloServer} = require("apollo-server-express");
+const {typeDefs,resolvers} = require('./Shcemas');
+const { authMiddleware } = require("./utils/auth");
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// add apollo server and schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
+// add apollo server and exprress as middleware
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,8 +25,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
+// add routes
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,"../client/build/index.html"));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
 });
